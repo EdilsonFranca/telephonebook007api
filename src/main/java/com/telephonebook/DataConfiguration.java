9c88a5afbf0b5f74af22827eb7c4e1941a441314
Edilson França <42967820+EdilsonFranca@users.noreply.github.com>
2022-07-30 23:24:15 -0300

    Update DataConfiguration.java

--- src/main/java/com/telephonebook/DataConfiguration.java
+++ src/main/java/com/telephonebook/DataConfiguration.java
@@ -12,24 +12,29 @@ import javax.sql.DataSource;
 @Configuration
 public class DataConfiguration {
 
-    @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/telephone-book-db?enabledTLSProtocols=TLSv1.2&useTimezone=true&serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("63286144");
-        return dataSource;
-    }
 
     @Bean
-    public JpaVendorAdapter jpaVendorAdapter(){
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.MYSQL);
-        adapter.setShowSql(true);
-        adapter.setGenerateDdl(true);
-        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
-        adapter.setPrepareConnection(true);
-        return adapter;
-    }
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://185.201.11.170:3306/telephone-book-db?enabledTLSProtocols=TLSv1.2&useTimezone=true&serverTimezone=UTC");
+		dataSource.setUsername("u979687582_delivery_admin");
+		dataSource.setPassword("6$Pap]?9vY8L");
+
+		dataSource.setUrl("jdbc:mysql://localhost:3306/u979687582_delivery_app?enabledTLSProtocols=TLSv1.2&useTimezone=true&serverTimezone=UTC");
+		dataSource.setUsername("root");
+		dataSource.setPassword("63286144");
+		return dataSource;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
 }
